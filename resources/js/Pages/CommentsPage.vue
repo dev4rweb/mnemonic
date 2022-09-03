727a58fda8d47702a4a9164320538fbd50fab94f
Vladislav <dev4rweb@gmail.com>
2022-09-03 10:27:47 +0300

    Adding content

--- resources/js/Pages/CommentsPage.vue
+++ resources/js/Pages/CommentsPage.vue
@@ -1,7 +1,12 @@
 <template>
-<div>
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, sequi!
-</div>
+    <div>
+
+        <h1>Comment page</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aspernatur delectus dolores doloribus
+            ducimus eveniet, facilis fuga, laborum odio quaerat quod rem sunt totam, veritatis vero! A accusantium autem
+            culpa delectus eligendi harum id inventore ipsa iusto laboriosam laborum nihil non, nulla officiis placeat
+            porro quae reprehenderit, sapiente sed veritatis!</p>
+    </div>
 </template>
 
 <script>
