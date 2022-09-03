31cd34a31f5b026f1090268f10a1f8f14bf90ee1
Vladislav <dev4rweb@gmail.com>
2022-09-03 10:23:41 +0300

    Adding commentsPage

---
+++ resources/js/Pages/CommentsPage.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, sequi!
+</div>
+</template>
+
+<script>
+export default {
+    name: "CommentsPage"
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/Pages/ContactsPage.vue
+++ resources/js/Pages/ContactsPage.vue
@@ -1,14 +1,20 @@
 <template>
-<Layout>
-    <Head title="Contacts Page" />
-    <div class="container">Contact Page</div>
-    <button class="btn btn-success"><i class="bi bi-3-circle-fill"></i> Button</button>
-</Layout>
+    <Layout>
+        <Head title="Contacts Page"/>
+        <div class="container">Contact Page</div>
+        <button class="btn btn-success"><i class="bi bi-3-circle-fill"></i> Button</button>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse est fuga minus quasi ullam voluptate! Error ex
+            iste provident quia.</p>
+        <div>
+            Some changes
+        </div>
+    </Layout>
 </template>
 
 <script>
 import {Head} from '@inertiajs/inertia-vue'
 import Layout from "../components/Layout";
+
 export default {
     name: "ContactsPage",
     components: {
