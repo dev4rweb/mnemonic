c7f200b88ed81bbbf60f984fe7ce5a914a5bc496
Vladislav <dev4rweb@gmail.com>
2022-09-03 10:36:11 +0300

    Adding content in Contact page

--- resources/js/Pages/ContactsPage.vue
+++ resources/js/Pages/ContactsPage.vue
@@ -1,14 +1,30 @@
 <template>
-<Layout>
-    <Head title="Contacts Page" />
-    <div class="container">Contact Page</div>
-    <button class="btn btn-success"><i class="bi bi-3-circle-fill"></i> Button</button>
-</Layout>
+    <Layout>
+        <Head title="Contacts Page"/>
+        <div class="container">Contact Page</div>
+        <button class="btn btn-success"><i class="bi bi-3-circle-fill"></i> Button</button>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci beatae deserunt dolorem illo ipsa iste
+            maiores ullam unde. Dolore enim hic nemo, officiis perspiciatis provident quae sed? Aliquam asperiores
+            aspernatur commodi, delectus doloribus ducimus earum explicabo labore, maiores necessitatibus neque nostrum
+            placeat quam qui quis saepe sit soluta ullam velit voluptatem? Ab aperiam architecto consectetur cumque
+            dignissimos dolorem earum expedita explicabo facere inventore laboriosam maxime natus perferendis
+            praesentium, quaerat, quia ratione reprehenderit sapiente sed tenetur ullam unde voluptates! Aspernatur est
+            itaque nostrum ratione tenetur? Assumenda atque, commodi deleniti distinctio in labore mollitia qui quod sed
+            ut! Consequuntur corporis dignissimos doloremque doloribus enim eum exercitationem facere fugiat fugit illum
+            in, iure laudantium modi natus neque nesciunt nisi nobis obcaecati perferendis porro provident quae quaerat
+            quasi quidem quisquam ratione repellat repellendus reprehenderit sequi temporibus vel, veniam, voluptate
+            voluptatibus? Distinctio ducimus earum natus nihil perspiciatis placeat tenetur voluptatum. Ab alias aut
+            earum, modi necessitatibus nemo non quod similique! Blanditiis cum doloribus incidunt ipsum minima
+            necessitatibus officiis perferendis quaerat quidem repellendus sit tempora temporibus, vel? Ab dolore eius
+            ex impedit laborum sequi voluptatum. Alias animi architecto asperiores blanditiis cum dignissimos dolores ea
+            illo ipsam, natus odio officiis perferendis, possimus quasi similique sint totam veritatis!</p>
+    </Layout>
 </template>
 
 <script>
 import {Head} from '@inertiajs/inertia-vue'
 import Layout from "../components/Layout";
+
 export default {
     name: "ContactsPage",
     components: {
