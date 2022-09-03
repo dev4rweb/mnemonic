d6aea20f0d07cd25c3707de88c404218e302ea87
Vladislav <dev4rweb@gmail.com>
2022-09-03 10:30:49 +0300

    Creating chat page

---
+++ resources/js/Pages/ChatPage.vue
@@ -0,0 +1,17 @@
+<template>
+<Layout>
+    <h1>Chat Page</h1>
+</Layout>
+</template>
+
+<script>
+import Layout from "../components/Layout";
+export default {
+    name: "ChatPage",
+    components: {Layout}
+}
+</script>
+
+<style scoped>
+
+</style>
