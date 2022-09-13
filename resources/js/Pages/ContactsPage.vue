195716a7ec46545218e65f7613e57fc227ea953f
Vladislav <dev4rweb@gmail.com>
2022-09-13 08:00:20 +0300

    Adding changes in master

--- resources/js/Pages/ContactsPage.vue
+++ resources/js/Pages/ContactsPage.vue
@@ -3,22 +3,12 @@
         <Head title="Contacts Page"/>
         <div class="container">Contact Page</div>
         <button class="btn btn-success"><i class="bi bi-3-circle-fill"></i> Button</button>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci beatae deserunt dolorem illo ipsa iste
-            maiores ullam unde. Dolore enim hic nemo, officiis perspiciatis provident quae sed? Aliquam asperiores
-            aspernatur commodi, delectus doloribus ducimus earum explicabo labore, maiores necessitatibus neque nostrum
-            placeat quam qui quis saepe sit soluta ullam velit voluptatem? Ab aperiam architecto consectetur cumque
-            dignissimos dolorem earum expedita explicabo facere inventore laboriosam maxime natus perferendis
-            praesentium, quaerat, quia ratione reprehenderit sapiente sed tenetur ullam unde voluptates! Aspernatur est
-            itaque nostrum ratione tenetur? Assumenda atque, commodi deleniti distinctio in labore mollitia qui quod sed
-            ut! Consequuntur corporis dignissimos doloremque doloribus enim eum exercitationem facere fugiat fugit illum
-            in, iure laudantium modi natus neque nesciunt nisi nobis obcaecati perferendis porro provident quae quaerat
-            quasi quidem quisquam ratione repellat repellendus reprehenderit sequi temporibus vel, veniam, voluptate
-            voluptatibus? Distinctio ducimus earum natus nihil perspiciatis placeat tenetur voluptatum. Ab alias aut
-            earum, modi necessitatibus nemo non quod similique! Blanditiis cum doloribus incidunt ipsum minima
-            necessitatibus officiis perferendis quaerat quidem repellendus sit tempora temporibus, vel? Ab dolore eius
-            ex impedit laborum sequi voluptatum. Alias animi architecto asperiores blanditiis cum dignissimos dolores ea
-            illo ipsam, natus odio officiis perferendis, possimus quasi similique sint totam veritatis!</p>
-        <p>Changed in master</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse est fuga minus quasi ullam voluptate! Error ex
+            iste provident quia.</p>
+        <p>Change in master</p>
+        <div>
+            Some changes
+        </div>
     </Layout>
 </template>
 
